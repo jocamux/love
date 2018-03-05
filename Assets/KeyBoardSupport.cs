84e6de754b979369a1bb4f3a45f515cc84730a76
JPoker98 <jocesmar@gmail.com>
2018-03-05 04:09:21 +0100

    UI centered + SPACE to start

---
+++ Assets/KeyBoardSupport.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBoardSupport : MonoBehaviour {
+
+    public SceneManagement sm;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            sm.GoToGameplay();
+        }
+	}
+}
